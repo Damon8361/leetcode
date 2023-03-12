a6037e8dcc48ff2dd2d75473e0b87211d1cfd924
Damon <121293121+Damon8361@users.noreply.github.com>
2023-03-13 00:22:35 +1000

    Add files via upload

---
+++ Leetcode_Pra/src/leetcode/E26.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class E26 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class SE_26 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int p = 0;
+        int q = 1;
+        while (q < nums.length) {
+            if (nums[p] != nums[q]) {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+    }
+
+}
